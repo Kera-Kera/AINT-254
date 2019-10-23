9705e27de8792ed776d402983ba21cb603ba5939
Kera-Kera <56071574+Kera-Kera@users.noreply.github.com>
2019-10-24 00:16:04 +0100

    Enemies destroyed on hit

--- Pinball 3D Smack Attack/Assets/Scripts/Ball/BallCamera.cs
+++ Pinball 3D Smack Attack/Assets/Scripts/Ball/BallCamera.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BallCamera : MonoBehaviour
 {
@@ -14,10 +15,38 @@ public class BallCamera : MonoBehaviour
 
     }
 
+<<<<<<< Updated upstream
     // Update is called once per frame
     void LateUpdate()
     {
         transform.position = ball.transform.position + distance;
+=======
+    private void Start()
+    {
+        Screen.SetResolution(1280 , 720, false);
+    }
+
+    void LateUpdate()
+    {
+        mouseX += Input.GetAxis("Mouse X") * 2;
+        mouseY -= Input.GetAxis("Mouse Y") * 4;
+        mouseY = Mathf.Clamp(mouseY, -40, 85);
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+
+
+
+        Cursor.visible = false;
+>>>>>>> Stashed changes
 
     }
 }
--- Pinball 3D Smack Attack/Assets/Scripts/Ball/BallJump.cs
+++ Pinball 3D Smack Attack/Assets/Scripts/Ball/BallJump.cs
@@ -15,7 +15,6 @@ public class BallJump : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-
     }
 
     // Update is called once per frame
@@ -23,6 +22,80 @@ public class BallJump : MonoBehaviour
     {
         int notPlayerMask = ~(1 << 8);
 
+<<<<<<< Updated upstream
+=======
+
+    
+            
+
+
+
+        isGrounded = Physics.CheckSphere(transform.position - (Vector3.up * 0.08f), 0.5f, notPlayerMask);
+
+        Collider[] objectColliders = Physics.OverlapSphere(transform.position, 7f);
+
+        for (int i = 0; i <= objectColliders.Length - 1; i++)
+        {
+            GameObject homingTarget = objectColliders[i].gameObject;
+            if (homingTarget.tag == "HomingTarget")
+            {
+
+
+
+                angle = Vector3.Angle(homingTarget.transform.position - transform.position, Camera.main.transform.forward);
+                {
+                    if (!usingHoming)
+                    {
+                        if (Mathf.Abs(angle) < 50)
+                        {
+
+                            
+                            
+
+                            if (storedHomingTarget == null)
+                            {
+                                storedHomingTarget = homingTarget;
+                            }
+
+                            //Physics.Raycast(transform.position, (transform.position - homingTarget.transform.position), out RaycastHit hit, 10f, notPlayerMask);
+
+                         //   Debug.DrawRay(transform.position, (transform.position - homingTarget.transform.position) * 10f, Color.yellow);
+
+                            //if (hit.transform.tag == homingTarget.tag)
+                           // {
+                          //  Debug.Log(hit.transform.gameObject);
+
+                                if (Vector3.Distance(transform.position, storedHomingTarget.transform.position) > Vector3.Distance(transform.position, homingTarget.transform.position))
+                                {
+
+                                    storedHomingTarget.GetComponent<HomingPassthrough>().IsNotSelected();
+                                    storedHomingTarget = homingTarget;
+
+                                }
+                          // }
+                        }
+                    }
+                }
+            }           
+        }
+
+        if (storedHomingTarget != null)
+        {
+            storedHomingTarget.GetComponent<HomingPassthrough>().IsSelected();
+            angle = Vector3.Angle(storedHomingTarget.transform.position - transform.position, Camera.main.transform.forward);
+            if (Vector3.Distance(transform.position, storedHomingTarget.transform.position) > 7f || (Mathf.Abs(angle) > 50))
+            {
+                storedHomingTarget.GetComponent<HomingPassthrough>().IsNotSelected();
+                storedHomingTarget = null;
+            }
+        }
+
+
+        /*  if(isGrounded) {
+         *    homingUsed=false;
+         *  }
+         */
+>>>>>>> Stashed changes
 
 
         isGrounded = Physics.CheckSphere(transform.position - (Vector3.up * 0.05f), 0.5f, notPlayerMask);
@@ -38,6 +111,36 @@ public class BallJump : MonoBehaviour
                 }
             }
         }
+<<<<<<< Updated upstream
+=======
+        if (usingHoming == true)
+        {
+            rb.velocity = Vector3.zero;
+            transform.position = Vector3.MoveTowards(transform.position, currentStoredHomingTarget.transform.position, 20f * Time.deltaTime);
+            
+            if (Vector3.Distance(transform.position , currentStoredHomingTarget.transform.position) < 0.6f)
+            {
+                rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+                StartCoroutine(Spawning(currentStoredHomingTarget));
+                storedHomingTarget.GetComponent<HomingPassthrough>().IsNotSelected();
+                storedHomingTarget = null;
+                homingUsed = false;
+                usingHoming = false;
+            }
+        }
+>>>>>>> Stashed changes
+    }
+
+    IEnumerator Spawning(GameObject currentcurrentStoredHomingTarget)
+    {
+        currentcurrentStoredHomingTarget.SetActive(false);
+        yield return StartCoroutine(WaitForSeconds(2.0F));
+        currentcurrentStoredHomingTarget.SetActive(true);
+    }
+
+    IEnumerator WaitForSeconds(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
     }
 }
   //  IEnumerator DelayedJumping()
